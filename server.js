54a10ee623fc28cda3fbdb9fccc45c6e3fbe4a44
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-09 16:49:17 -0400

    able to send data from back-end to front-end

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import * as dotenv from 'dotenv'
 import db from "./db/dbConnection.js";
 import questionsRoutes from "./routes/questionsRoutes.js";
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 
