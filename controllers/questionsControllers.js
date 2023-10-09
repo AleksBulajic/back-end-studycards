6a6e04c829bb41d03d016ac07564d40d65d697a6
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-09 14:28:49 -0400

    able to get data thru postman

--- controllers/questionsControllers.js
+++ controllers/questionsControllers.js
@@ -5,6 +5,7 @@ export const getQuestions = async (req, res) => {
     try{
         const questions = await FlashCards.find();
         res.status(200).json(questions);
+        console.log(questions);
     }catch(err){
         res.status(500).json({message: err.message})
     }
